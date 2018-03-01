960647a610646267f049aa678f756968437c0865
Teemu Suoranta <teemu.suoranta@aucor.fi>
2018-03-01 08:55:13 +0200

    Remove content-none for now

--- htdocs/wp-content/themes/mita/partials/content-none.php
+++ htdocs/wp-content/themes/mita/partials/content-none.php
@@ -5,16 +5,4 @@
  * @package mita
  */
 
-?>
-
-<article class="error-404 not-found">
-  <div class="page-content">
-    <h1 class="entry-title"><?php ask_e('Search: Nothing found'); ?></h1>
-    <div class="entry-content">
-      <p><?php ask_e('Search: Nothing found description'); ?></p>
-      <div class="search-form search-404">
-        <?php get_search_form(); ?>
-      </div>
-    </div>
-  </div><!-- .page-content -->
-</article><!-- .error-404 -->
+// say nothing...
